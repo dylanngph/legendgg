8b68dc6f526032da51f28b88c47633e89039c8ec
Thap Nguyen <tomhalan@jadelabs.org>
2022-02-14 11:44:47 +0700

    style container contact

--- src/screens/contact/index.jsx
+++ src/screens/contact/index.jsx
@@ -67,110 +67,112 @@ function ContactScreen() {
         <Typography variant='h1' sx={{ padding: { md: '15px 0', xs: '0 0 15px' }, fontWeight: 'bold', fontSize: { md: '38px', xs: '26px' } }}>Contact Us</Typography>
         <div>We are ready to lead you into the future</div>
       </Box>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Box sx={{ maxWidth: '100%', margin: '0 auto', width: { sm: '400px', xs: 'inherit'} }}>
-            <BoxInfo>
-              <BoxTitle>
-                <LocationIcon />
-                <Box sx={{ marginLeft: '10px'}}>Address</Box>
-              </BoxTitle>
-              <BoxContent>138 Robinson Road #02-50 Singapore 068906</BoxContent>
-            </BoxInfo>
-            <BoxInfo>
-              <BoxTitle>
-                <CallIcon />
-                <Box sx={{ marginLeft: '10px'}}>Phone</Box>
-              </BoxTitle>
-              <BoxContent><a href="tel:6596416855">+65 9641 6855</a></BoxContent>
-            </BoxInfo>
-            <BoxInfo>
-              <BoxTitle>
-                <SMSIcon />
-                <Box sx={{ marginLeft: '10px'}}>Email</Box>
-              </BoxTitle>
-              <BoxContent>Email: <a href="mailto:hi@cgstudio.org">hi@cgstudio.org</a></BoxContent>
-            </BoxInfo>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-        {!stateSuccess && (
-          <Box sx={{ backgroundColor: '#f5f5f5', borderRadius: '6px', padding: '24px', maxWidth: '100%', margin: '0 auto', width: { md: '400px'} }}>
-            <FieldBox sx={{width: '100%'}}>
-              <LabelBox sx={{fontWeight: '600'}}>First Name</LabelBox>
+      <Box sx={{ maxWidth: {lg: '900px'}, margin: '0 auto' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ maxWidth: '100%', margin: '0 auto', width: { sm: '400px', xs: 'inherit'} }}>
+              <BoxInfo>
+                <BoxTitle>
+                  <LocationIcon />
+                  <Box sx={{ marginLeft: '10px'}}>Address</Box>
+                </BoxTitle>
+                <BoxContent>138 Robinson Road #02-50 Singapore 068906</BoxContent>
+              </BoxInfo>
+              <BoxInfo>
+                <BoxTitle>
+                  <CallIcon />
+                  <Box sx={{ marginLeft: '10px'}}>Phone</Box>
+                </BoxTitle>
+                <BoxContent><a href="tel:6596416855">+65 9641 6855</a></BoxContent>
+              </BoxInfo>
+              <BoxInfo>
+                <BoxTitle>
+                  <SMSIcon />
+                  <Box sx={{ marginLeft: '10px'}}>Email</Box>
+                </BoxTitle>
+                <BoxContent>Email: <a href="mailto:hi@cgstudio.org">hi@cgstudio.org</a></BoxContent>
+              </BoxInfo>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          {!stateSuccess && (
+            <Box sx={{ backgroundColor: '#f5f5f5', borderRadius: '6px', padding: '24px', maxWidth: '100%', margin: '0 auto', width: { md: '400px'} }}>
+              <FieldBox sx={{width: '100%'}}>
+                <LabelBox sx={{fontWeight: '600'}}>First Name</LabelBox>
+                  <WrapperInput>
+                    <TextField
+                      fullWidth
+                      name="firstName"
+                      value={values.firstName}
+                      onChange={handleInputChange}
+                      placeholder="Enter your first name"
+                      variant="outlined"
+                    />
+                  </WrapperInput>
+              </FieldBox>
+              <FieldBox sx={{width: '100%'}}>
+                <LabelBox sx={{fontWeight: '600'}}>Last Name</LabelBox>
                 <WrapperInput>
                   <TextField
                     fullWidth
-                    name="firstName"
-                    value={values.firstName}
+                    name="lastName"
+                    value={values.lastName}
                     onChange={handleInputChange}
-                    placeholder="Enter your first name"
+                    placeholder="Enter your last name"
                     variant="outlined"
                   />
                 </WrapperInput>
-            </FieldBox>
-            <FieldBox sx={{width: '100%'}}>
-              <LabelBox sx={{fontWeight: '600'}}>Last Name</LabelBox>
-              <WrapperInput>
-                <TextField
-                  fullWidth
-                  name="lastName"
-                  value={values.lastName}
-                  onChange={handleInputChange}
-                  placeholder="Enter your last name"
-                  variant="outlined"
-                />
-              </WrapperInput>
-            </FieldBox>
-            <FieldBox sx={{width: '100%'}}>
-              <LabelBox sx={{fontWeight: '600'}}>Email</LabelBox>
-              <WrapperInput>
-                <TextField
-                  fullWidth
-                  name="email"
-                  value={values.email}
-                  onChange={handleInputChange}
-                  placeholder="Enter your email"
-                  variant="outlined"
-                />
-              </WrapperInput>
-            </FieldBox>
-            <FieldBox sx={{width: '100%'}}>
-              <LabelBox sx={{fontWeight: '600'}}>Message</LabelBox>
-              <WrapperInput>
-                <TextareaAutosize
-                  aria-label="minimum height"
-                  minRows={4}
-                  placeholder="Enter your message"
-                  style={{ width: 200 }}
-                  value={values.massage}
-                  name="massage"
-                  onChange={handleInputChangeTextarea}
-                />
-              </WrapperInput>
-            </FieldBox>
-            <FieldBox sx={{ marginTop: '20px', width: '100%' }}>
-              {error && <Box sx={{ marginBottom: '10px' }}><Alert severity="error">{error}</Alert></Box>}
-              <CustomButton 
-                variant="contained"
-                onClick={() => handleSend(values)}
-                className={loading ? 'loading-btn' : ''}
-              >
-                {!loading ? 'Send' : (<Fade
-                  in={loading}
-                  unmountOnExit
+              </FieldBox>
+              <FieldBox sx={{width: '100%'}}>
+                <LabelBox sx={{fontWeight: '600'}}>Email</LabelBox>
+                <WrapperInput>
+                  <TextField
+                    fullWidth
+                    name="email"
+                    value={values.email}
+                    onChange={handleInputChange}
+                    placeholder="Enter your email"
+                    variant="outlined"
+                  />
+                </WrapperInput>
+              </FieldBox>
+              <FieldBox sx={{width: '100%'}}>
+                <LabelBox sx={{fontWeight: '600'}}>Message</LabelBox>
+                <WrapperInput>
+                  <TextareaAutosize
+                    aria-label="minimum height"
+                    minRows={4}
+                    placeholder="Enter your message"
+                    style={{ width: 200 }}
+                    value={values.massage}
+                    name="massage"
+                    onChange={handleInputChangeTextarea}
+                  />
+                </WrapperInput>
+              </FieldBox>
+              <FieldBox sx={{ marginTop: '20px', width: '100%' }}>
+                {error && <Box sx={{ marginBottom: '10px' }}><Alert severity="error">{error}</Alert></Box>}
+                <CustomButton 
+                  variant="contained"
+                  onClick={() => handleSend(values)}
+                  className={loading ? 'loading-btn' : ''}
                 >
-                  <CircularProgress sx={{ color: '#ffffff', height: '20px!important', width: '20px!important',  }} />
-                </Fade>)}
-              </CustomButton>
-            </FieldBox>
-          </Box>
-        )}
-        {stateSuccess && (
-          <Alert severity="success">Sent successfully</Alert>
-        )}
+                  {!loading ? 'Send' : (<Fade
+                    in={loading}
+                    unmountOnExit
+                  >
+                    <CircularProgress sx={{ color: '#ffffff', height: '20px!important', width: '20px!important',  }} />
+                  </Fade>)}
+                </CustomButton>
+              </FieldBox>
+            </Box>
+          )}
+          {stateSuccess && (
+            <Alert severity="success">Sent successfully</Alert>
+          )}
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   )
 }
