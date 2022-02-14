7c12245d317e614b21f00aae7c74e71339af30ce
Thap Nguyen <tomhalan@jadelabs.org>
2022-02-14 14:42:59 +0700

    style page my account

--- src/screens/myAccount/index.jsx
+++ src/screens/myAccount/index.jsx
@@ -18,7 +18,7 @@ function MyAccountScreen() {
   }, []);
 
   return (
-    <Box>
+    <Box sx={{padding: '25px'}}>
       <Box sx={{textAlign: 'center', marginBottom: '60px'}}>
         <Typography variant='h1' sx={{ padding: { md: '15px 0', xs: '0 0 15px' }, fontWeight: 'bold', fontSize: { md: '38px', xs: '26px' } }}>Thông tin của tôi</Typography>
         <Box sx={{marginBottom: '10px'}}>{user.email}</Box>
