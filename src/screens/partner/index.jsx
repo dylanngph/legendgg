6a161daa4346b1ce7d8faf2123a5811957e38321
Thap Nguyen <tomhalan@jadelabs.org>
2022-02-14 14:28:42 +0700

    refactor code

--- src/screens/partner/index.jsx
+++ src/screens/partner/index.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, Skeleton } from '@mui/material';
 import partnerApi from 'api/partnerApi';
 
 function PartnerScreen() {
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [listPartner, setListPartner] = useState([]);
 
   useEffect(() => {
     const listPartner = async () => {
-      // setLoading(true);
+      setLoading(true);
       try {
         const response = await partnerApi.getAll();
         setListPartner(response.data);
-        // setLoading(false);
-      } catch (error) { }
+        setLoading(false);
+      } catch (error) { setLoading(false); }
     }
     listPartner();
   }, []);
@@ -24,18 +24,38 @@ function PartnerScreen() {
         <Typography variant='h1' sx={{ padding: { md: '15px 0', xs: '0 0 15px' }, fontWeight: 'bold', fontSize: { md: '38px', xs: '26px' } }}> Our Partners </Typography>
         <div>We’ve integrated with the below partners and more.</div>
       </Box>
-      {listPartner.length > 0 && (
+      {loading && (
         <Grid container justifyContent='center' spacing={2}>
-        {listPartner.map((partner, index) => (
-          <Grid key={index} item xs={6} sm={4} md={2.4}>
-            <a href={partner.website} target='_blank' rel="noreferrer">
-              <Box sx={{ '& img': {width: '100%', maxWidth: '100%', height: 'auto'} }}>
-                <img src={partner.logo} alt={partner.name} />
-              </Box>
-            </a>
+          <Grid item xs={6} sm={4} md={2.4}>
+            <Skeleton animation="wave" variant="rectangular" width="100%" height={120} />
           </Grid>
-        ))}
+          <Grid item xs={6} sm={4} md={2.4}>
+            <Skeleton animation="wave" variant="rectangular" width="100%" height={120} />
+          </Grid>
+          <Grid item xs={6} sm={4} md={2.4}>
+            <Skeleton animation="wave" variant="rectangular" width="100%" height={120} />
+          </Grid>
+          <Grid item xs={6} sm={4} md={2.4}>
+            <Skeleton animation="wave" variant="rectangular" width="100%" height={120} />
+          </Grid>
         </Grid>
+      )}
+      {!loading && (
+        <>
+        {listPartner.length > 0 && (
+          <Grid container justifyContent='center' spacing={2}>
+          {listPartner.map((partner, index) => (
+            <Grid key={index} item xs={6} sm={4} md={2.4}>
+              <a href={partner.website} target='_blank' rel="noreferrer">
+                <Box sx={{ '& img': {width: '100%', maxWidth: '100%', height: 'auto'} }}>
+                  <img src={partner.logo} alt={partner.name} />
+                </Box>
+              </a>
+            </Grid>
+          ))}
+          </Grid>
+        )}
+        </>
       )}
     </Box>
   )
