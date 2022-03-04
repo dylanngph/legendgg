d2a07e1fd1ca817520dd89300b1ec4ee154b21cd
Thap Nguyen <tomhalan@jadelabs.org>
2022-03-04 14:53:55 +0700

    feat change phone and email contact

--- src/constants/data/contact/index.js
+++ src/constants/data/contact/index.js
@@ -1,5 +1,5 @@
 export const CONTACT_SETTING = {
   address: '138 Robinson Road #02-50 Singapore 068906',
-  email: 'hi@cgstudio.org',
-  phone: '+65 9641 6855',
+  email: 'legendguildgame@gmail.com',
+  phone: '0397392810',
 }
