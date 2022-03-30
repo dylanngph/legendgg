ab3bdb377f6d2d6dab0de6fd225329514b7cfd6d
Claris <claris@jadelabs.org>
2022-03-30 16:36:11 +0700

    add invest

--- src/screens/partner/index.jsx
+++ src/screens/partner/index.jsx
@@ -6,6 +6,19 @@ function PartnerScreen() {
   const [loading, setLoading] = useState(false);
   const [listPartner, setListPartner] = useState([]);
 
+  const listInvest = [
+    {
+      src: 'images/Jadelabs_logo.jpg',
+      name: 'Jadelabs',
+      url: 'https://www.jadelabs.org/'
+    },
+    {
+      src: 'images/trustpaygroup_logo.png',
+      name: 'TPH ventures',
+      url: 'https://trustpaygroup.com/'
+    }
+  ]
+
   useEffect(() => {
     const listPartner = async () => {
       setLoading(true);
@@ -19,8 +32,24 @@ function PartnerScreen() {
   }, []);
 
   return (
-    <Box sx={{padding: '25px'}}>
-      <Box sx={{textAlign: 'center', marginBottom: '60px'}}>
+    <Box sx={{ padding: '25px' }}>
+      <Box sx={{ textAlign: 'center', marginBottom: '40px' }}>
+        <Typography variant='h1' sx={{ padding: { md: '15px 0', xs: '0 0 15px' }, fontWeight: 'bold', fontSize: { md: '38px', xs: '26px' } }}>Cố vấn của chúng tôi</Typography>
+      </Box>
+
+      <Grid container justifyContent='center' alignItems='center' spacing={10} sx={{ marginBottom: '60px' }}>
+        {listInvest.map((invest, index) => (
+          <Grid key={index} item xs={6} sm={4} md={2.8}>
+            <a href={invest.url} target='_blank' rel="noreferrer">
+              <Box sx={{ '& img': { width: '100%', maxWidth: '100%', height: 'auto' } }}>
+                <img src={invest.src} alt={invest.name} />
+              </Box>
+            </a>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Box sx={{ textAlign: 'center', marginBottom: '40px' }}>
         <Typography variant='h1' sx={{ padding: { md: '15px 0', xs: '0 0 15px' }, fontWeight: 'bold', fontSize: { md: '38px', xs: '26px' } }}>Đối tác của chúng tôi</Typography>
         <div>Chúng tôi đã hợp tác với các đối tác bên dưới và hơn thế nữa.</div>
       </Box>
@@ -42,19 +71,19 @@ function PartnerScreen() {
       )}
       {!loading && (
         <>
-        {listPartner.length > 0 && (
-          <Grid container justifyContent='center' alignItems='center' spacing={2}>
-          {listPartner.map((partner, index) => (
-            <Grid key={index} item xs={6} sm={4} md={2.4}>
-              <a href={partner.website} target='_blank' rel="noreferrer">
-                <Box sx={{ '& img': {width: '100%', maxWidth: '100%', height: 'auto'} }}>
-                  <img src={partner.logo} alt={partner.name} />
-                </Box>
-              </a>
+          {listPartner.length > 0 && (
+            <Grid container justifyContent='center' alignItems='center' spacing={2}>
+              {listPartner.map((partner, index) => (
+                <Grid key={index} item xs={6} sm={4} md={2.4}>
+                  <a href={partner.website} target='_blank' rel="noreferrer">
+                    <Box sx={{ '& img': { width: '100%', maxWidth: '100%', height: 'auto' } }}>
+                      <img src={partner.logo} alt={partner.name} />
+                    </Box>
+                  </a>
+                </Grid>
+              ))}
             </Grid>
-          ))}
-          </Grid>
-        )}
+          )}
         </>
       )}
     </Box>
